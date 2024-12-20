4c49d3d9ea9e68a54ad1f9ad4d86fd6e06455c5d
JeremyDeandito <deanditojeremy@gmail.com>
2024-12-21 01:21:19 +0700

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TST-Final-Project-JeremyD/',
   server: {
     cors: true,
     headers: {
